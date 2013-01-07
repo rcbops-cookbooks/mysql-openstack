61c7094b515aebf92e05e9d78fcae5e1255dbf6f
Darren Birkett <darren.birkett@gmail.com>
2013-01-07 15:49:38 +0000

    first attempt at master/master replication

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,16 @@ default["mysql"]["services"]["db"]["scheme"] = "tcp"        # node_attribute
 default["mysql"]["services"]["db"]["port"] = 3306           # node_attribute
 default["mysql"]["services"]["db"]["network"] = "nova"      # node_attribute
 
+
+# because of some oddness with bug 993663, we seem to not like the default
+# charset to be utf8, but latin-1 instead.
+override["mysql"]["tunable"]["character-set-server"] = "latin1"
+override["mysql"]["tunable"]["collation-server"] = "latin1_general_ci"
+
+override["mysql"]["tunable"]["log_bin"] = "mysql-binlog"
+override["mysql"]["auto-increment-increment"] = "2"
+
+
 case platform
 when "fedora", "redhat", "centos", "scientific", "amazon"
   default["mysql"]["platform"] = {                          # node_attribute
--- recipes/server.rb
+++ recipes/server.rb
@@ -16,28 +16,167 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# replication parts inspired by https://gist.github.com/1105416
+
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
 
 include_recipe "osops-utils"
 include_recipe "monitoring"
+include_recipe "mysql::ruby"
 
 # Lookup endpoint info, and properly set mysql attributes
 mysql_info = get_bind_endpoint("mysql", "db")
-node.set["mysql"]["bind_address"] = mysql_info["host"]
+#node.set["mysql"]["bind_address"] = mysql_info["host"]
+bind_ip = get_ip_for_net("nova")
+node.set["mysql"]["bind_address"] = bind_ip
 
-# override this default attribute in the upstream mysql cookbooke
+# override default attributes in the upstream mysql cookbook
 if platform?(%w{redhat centos amazon scientific})
     node.override["mysql"]["tunable"]["innodb_adaptive_flushing"] = false
 end
 
-# because of some oddness with bug 993663, we seem to not like the default
-# charset to be utf8, but latin-1 instead.
-node.override["mysql"]["tunable"]["character-set-server"] = "latin1"
-node.override["mysql"]["tunable"]["collation-server"] = "latin1_general_ci"
+# generate mysql server_id from my ip address
+node.override["mysql"]["tunable"]["server_id"] = get_ip_for_net("nova").gsub(/\./, '')
 
-# install the mysql gem
-include_recipe "mysql::ruby"
-# install the mysql server
-include_recipe "mysql::server"
+# search for first_master id (1).  If found, assume we are the second server
+# and configure accordingly.  If not, set our own and# assume we are the first
+
+if node["mysql"]["myid"].nil?
+  # then we have not yet been through setup - try and find first master
+  masters = search(:node, "chef_environment:#{node.chef_environment} AND mysql_myid:1")
+
+  if masters.length == 0
+    # we must be first master
+    Chef::Log.info("*** I AM FIRST MYSQL MASTER ***")
+    if node["developer_mode"]
+      node.set_unless["mysql"]["tunable"]["repl_pass"] = "replication"
+    else
+      node.set_unless["mysql"]["tunable"]["repl_pass"] = secure_password
+    end
+
+    node.set["mysql"]["auto-increment-offset"] = "1"
+
+    #now we have set the necessary tunables, install the mysql server
+    include_recipe "mysql::server"
+
+    # since we are first master, create the replication user
+    mysql_connection_info = {:host => bind_ip , :username => 'root', :password => node['mysql']['server_root_password']}
+
+    mysql_database_user 'repl' do
+      connection mysql_connection_info
+      password node["mysql"]["tunable"]["repl_pass"]
+      action :create
+    end
+
+    mysql_database_user 'repl' do
+      connection mysql_connection_info
+      privileges ['REPLICATION SLAVE']
+      action :grant
+      host '%'
+    end
+
+    # set this last so we can only be found when we are finished
+    node.set_unless["mysql"]["myid"] = "1"
+
+  elsif masters.length == 1
+    # then we are second master
+    Chef::Log.info("*** I AM SECOND MYSQL MASTER ***")
+    first_master = masters.first
+    node.set_unless["mysql"]["tunable"]["repl_pass"] = first_master["mysql"]["tunable"]["repl_pass"]
+
+    node.set["mysql"]["tunable"]["auto-increment-offset"] = "2"
+
+    #now we have set the necessary tunables, install the mysql server
+    include_recipe "mysql::server"
+
+    first_master_ip = get_ip_for_net('nova', first_master)
+    # connect to master
+    ruby_block "configure slave" do
+      block do
+        require 'rubygems'
+        Gem.clear_paths
+        require 'mysql'
+
+        mysql_conn = Mysql.new(bind_ip, "root", node["mysql"]["server_root_password"])
+        command = %Q{
+        CHANGE MASTER TO
+          MASTER_HOST="#{first_master_ip}",
+          MASTER_USER="repl",
+          MASTER_PASSWORD="#{node["mysql"]["tunable"]["repl_pass"]}",
+          MASTER_LOG_FILE="#{node["mysql"]["tunable"]["log_bin"]}.000001",
+          MASTER_LOG_POS=0;
+          }
+          Chef::Log.info "Sending start replication command to mysql: "
+          Chef::Log.info "#{command}"
+
+        mysql_conn.query("stop slave")
+        mysql_conn.query(command)
+        mysql_conn.query("start slave")
+
+      end
+    end
+
+    # set this last so we can only be found when we are finished
+    node.set_unless["mysql"]["myid"] = 2
+
+  elsif masters.length > 1
+    # error out here as something is wrong
+    Chef::Application.fatal! "I discovered multiple mysql first masters - there can be only one!"
+
+  end
+
+end
+
+if node['mysql']['myid'] == '1'
+  # we were the first master, but have we connected back to the second master yet?
+  second_master = search(:node, "chef_environment:#{node.chef_environment} AND mysql_myid:2")
+
+  if second_master == 1
+    Chef::Log.info("I am the first master, and I have found the second master")
+    Chef::Log.info("Attempting to connect back to second master as a slave")
+
+    second_master_ip = get_ip_for_net('nova', second_master[0])
+
+    # attempt to connect to second master as a slave
+    ruby_block "configure slave" do
+      block do
+        require 'rubygems'
+        Gem.clear_paths
+        require 'mysql'
+
+        mysql_conn = Mysql.new(bind_ip, "root", node["mysql"]["server_root_password"])
+        command = %Q{
+        CHANGE MASTER TO
+          MASTER_HOST="#{second_master_ip}",
+          MASTER_USER="repl",
+          MASTER_PASSWORD="#{node["mysql"]["tunable"]["repl_pass"]}",
+          MASTER_LOG_FILE="#{node["mysql"]["tunable"]["log_bin"]}.000001",
+          MASTER_LOG_POS=0;
+          }
+        Chef::Log.info "Sending start replication command to mysql: "
+        Chef::Log.info "#{command}"
+
+        mysql_conn.query("stop slave")
+        mysql_conn.query(command)
+        mysql_conn.query("start slave")
+      end
+
+      not_if do
+        #TODO this fails if mysql is not running - check first
+        mysql_conn = Mysql.new(bind_ip, "root", node["mysql"]["server_root_password"])
+        slave_sql_running = ""
+        mysql_conn.query("show slave status") {|r| r.each_hash {|h| slave_sql_running = h['Slave_SQL_Running'] } }
+        slave_sql_running == "Yes"
+      end
+
+    end
+  else
+  Chef::Log.info("I am the first master, but the second master does not exist yet")
+  end
+end
+
+
+
 
 # Cleanup the craptastic mysql default users
 cookbook_file "/tmp/cleanup_anonymous_users.sql" do
