22bac8bb1cc4718497507fdc96a3454a33ffd0d8
Matt Thompson <mattt@defunct.ca>
2013-02-15 13:37:04 +0000

    Clean up README / metadata

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ license           "Apache 2.0"
 description       "Makes the mysql cookbook behave correctly with OpenStack"
 version           "1.0.12"
 
-%w{ ubuntu fedora redhat centos }.each do |os|
+%w{ centos ubuntu }.each do |os|
   supports os
 end
 
-%w{ database keepalived monitoring mysql osops-utils }.each do |dep|
+%w{ database keepalived monitoring mysql openssl osops-utils }.each do |dep|
   depends dep
 end
