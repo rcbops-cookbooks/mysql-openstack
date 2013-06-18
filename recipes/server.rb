50b3ca29b3b6cdcaabbe48b63af95ff08e87bd1b
Darren Birkett <darren.birkett@gmail.com>
2013-06-18 13:45:21 +0100

    set service network to management, and use that for the vip in ha mode

--- recipes/server.rb
+++ recipes/server.rb
@@ -213,7 +213,7 @@ if rcb_safe_deref(node, "vips.mysql-db")
   include_recipe "keepalived"
   vip = node["vips"]["mysql-db"]
   vrrp_name = "vi_#{vip.gsub(/\./, '_')}"
-  vrrp_network = node["mysql"]["services"]["db"]["vip_network"]
+  vrrp_network = node["mysql"]["services"]["db"]["network"]
   vrrp_interface = get_if_for_net(vrrp_network, node)
   router_id = node["mysql"]["ha"]["vrid"]
 
